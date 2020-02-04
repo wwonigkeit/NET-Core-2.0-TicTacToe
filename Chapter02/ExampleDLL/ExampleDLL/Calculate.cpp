7bd333c890d2c91f4d8dfb67611da523792b4c85
Wilhelm Wonigkeit <wwonigkeit@iqconsult.com.au>
2020-02-04 14:21:09 +1100

    Big Change remove everything

--- Chapter02/ExampleDLL/ExampleDLL/Calculate.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "Calculate.h"
-
-
-int main()
-{
-	int a = 2; int b = 5;
-	int result = Sum(a, b);
-}
--- Chapter06/FaqBot/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FaqBot.WebApiApplication" Language="C#" %>
